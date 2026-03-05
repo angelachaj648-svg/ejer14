636427e1866bb02f596daf3c6f8460dac84924d1
angela chaj <angelachaj648@gmail.com>
2026-03-05 17:50:54 -0600

    agregue if

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int numero;
+int mayor = int.Parse(Console.ReadLine()); ;
+
+do
+{
+	Console.Write("Ingrese un número (0 para terminar): ");
+	numero = int.Parse(Console.ReadLine());
+
+	if (numero > mayor)
+	{
+		mayor = numero;
+	}
+
+} while (numero != 0);
+
+Console.WriteLine("El número mayor es: " + mayor);
